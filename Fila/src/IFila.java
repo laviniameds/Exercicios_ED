17fc91cea3bf9e85f47d96b2a0af4871f11eda3f
Lavinia Medeiros Miranda <laviniameds@icloud.com>
2018-03-07 18:49:33 -0300

    ->Fila com array criada;

---
+++ Fila/src/IFila.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author lavinia
+ */
+public interface IFila {
+    public int tamanho();
+    public boolean estaVazia();
+    public Object inicio() throws EFilaVazia;
+    public void enfileirar(Object o);
+    public Object desenfileirar() throws EFilaVazia;
+}
