d82e31acbc715ad87a123af93b470ca2a771580b
Lavinia Medeiros Miranda <laviniameds@icloud.com>
2018-05-17 18:32:06 -0300

    codigo comentado;

--- HashTable/src/hashtable/HashTable.java
+++ HashTable/src/hashtable/HashTable.java
@@ -10,13 +10,17 @@ package hashtable;
  * @author lavinia
  */
 public class HashTable {
+    //tabela
     Hash[] table;
+    
+    //variaveis auxiliares
     int i, p, size;
     Object NO_SUCH_KEY = "Nenhuma chave encontrada!";
-    Object AVAIABLE = "Disponível";
+    Object AVAILABLE = "Disponível";
     
     public HashTable(int tam){
-        table = new Hash[tam];
+        //cria a table acom o dobro de espaços para chaves
+        table = new Hash[tam*2];
     }
     
     public void put(int key, Object element){
@@ -24,13 +28,19 @@ public class HashTable {
     }
     
     public Object find(int key){
+        //variavel q percorre o array
         i = table[key].getKey();
+        //variavel auxiliar para nao deixar que o array seja percorrido mais de uma vez
         p = 0;
+        //enquanto o array nao for percorrido 
         while(p != size){
+            //percorreu todo o array e nao achou a chave, para e retorna
             if(table[key] == null)
                 return NO_SUCH_KEY;
+            //achou a chave, retorna o elemento
             else if(i == key)
                 return table[key].getElement();
+            //nao achou a chave e o array n foi percorrido todo ainda, vai pro proximo espaço, aumenta a variavel auxiliar p
             else{
                 i = (i + 1) % size;
                 p++;
@@ -40,11 +50,14 @@ public class HashTable {
     }
     
     public Object remove(int key){
+        //pega o objeto
         Object o = find(key);
         
+        //se existir objeto, substitui o elemento por AVAIABLE
         if(o != NO_SUCH_KEY)          
-            table[key].setElement(AVAIABLE);
+            table[key].setElement(AVAILABLE);
         
+        //retorna o objeto substituído ou NO_SUCH_KEY
         return o;
     }
     
